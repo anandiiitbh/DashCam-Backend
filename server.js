bc246154d0fb252fcb9ceadd7c1088819563054a
anand_kumar <61147494+anandiiitbh@users.noreply.github.com>
2021-02-21 10:56:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ const rl = readline.createInterface({
 
 
 // start server
-const port = process.env.NODE_ENV === 'production' ? 80 : 4000;
+const port = process.env.NODE_ENV === 'production' ? 80 : 6000;
 const server = app.listen(port, function() {
     console.log('Server listening on port ' + port);
 
